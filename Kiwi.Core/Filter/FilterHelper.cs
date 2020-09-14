5be0bff053580257ff3e6c37fca130abb2ec5161
ERKAN TAYLAN <etaylan@pm.me>
2020-09-15 02:49:08 +0300

    filter

---
+++ Kiwi.Core/Filter/FilterHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kiwi.Core.Filter
+{
+    public static class FilterHelper
+    {
+        public static bool Contains(string filter, IEnumerable<string> items)
+        {
+            filter = filter.ToLower();
+            return (from i in items where i != null select i.ToLower()).Any(item => item.Contains(filter));
+        }
+
+        public static bool Contains(IEnumerable<string> filters, IList<string> items)
+        {
+            return filters.Select(f => Contains(f, items)).Any(contains => contains);
+        }
+
+        public static IEnumerable<string> SplitFilter(string filter)
+        {
+            return filter.Split(
+                new[]
+                {
+                    ' ',
+                    ','
+                },
+                StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
---
+++ Kiwi.Core/Filter/FilterableViewModelBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Prism.Mvvm;
+
+namespace Kiwi.Core.Filter
+{
+    public abstract class FilterableViewModelBase : BindableBase, IFilterable
+    {
+        private string search;
+
+        protected FilterableViewModelBase()
+        {
+            PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == nameof(Search))
+                {
+                    ChangeFilter();
+                }
+            };
+        }
+
+        public virtual string Search
+        {
+            get => search;
+            set => SetProperty(ref search, value);
+        }
+
+        /// <inheritdoc />
+        public Action<Predicate<object>> SetFilter { get; set; }
+
+        protected virtual void ChangeFilter()
+        {
+            if (Search.Length == 0)
+            {
+                SetFilter?.Invoke(null);
+            }
+            else
+            {
+                SetFilter?.Invoke(Filter);
+            }
+        }
+
+        protected abstract bool Filter(object o);
+    }
+}
